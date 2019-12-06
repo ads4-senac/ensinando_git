d40542b4b4aa655a7de0966a0a74e0e84984c616
Alex Ferreira <arf92@live.com>
2019-12-05 21:22:52 -0300

    Adicao de activity home

---
+++ ProjetoAndroid/app/src/main/java/senac/go/projetoandroid/TelaHome.java
@@ -0,0 +1,14 @@
+package senac.go.projetoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_home);
+    }
+}
